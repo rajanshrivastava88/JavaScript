f5dde71abba308f2581eaca1287a8d517685503c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-14 02:53:36 +0530

    Create 05_Datatypes-Summery.js

---
+++ 05_Datatypes-Summery.js
@@ -0,0 +1,81 @@
+/* There are 2 Type of Datatypes
+
+1. Primitive
+2. Non-Primitive
+
+
+
+
+Ques : Is JavaScript a statically or a dynamically typed language?
+
+Answer:: JavaScript is a dynamically typed language. but TypeScript is a statically typed language.
+
+
+*/
+
+//  Primitive Datatypes
+
+//  7 types : String, Number, Boolean, null(empty), undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n  // biginit datatype 
+
+
+
+// Reference Type (Non primitive datatype) => it is called by indexing in array and it is 3 types
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; // array
+
+let myObj = {    
+    name: "Rajan",
+    age: 24
+}  // Objects
+
+const myFunction = function(){
+    console.log("Hello world");
+}  // Functions
+
+//console.log(typeof anotherId, typeof myObj, typeof heros, typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ========================================================================
+
+// Memory 2 types 
+// 1. Stack Type or Stack Memory (Primitive) stack me value ka copy milta hai
+// 2. Heap Type or Heap Memory (Non Primitive) heap me value ka reference milta hai
+
+let myYoutubename = "RajanSrivastava"
+
+let anothername = myYoutubename  // in this we are using copy of myYoutubename
+anothername="Chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = {
+    email: "user@google.com",    
+    upi: "user@ybl"
+}
+
+let userTwo = userOne  // in this we are using the reference value of userOne 
+
+userTwo.email = "rajan@google.com" // userTwo.email (.email) is used for accessing the email value
+
+console.log(userOne.email);
+console.log(userTwo.email);
