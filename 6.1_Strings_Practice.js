f64878cf9db2ed04fb8c1132891b99d1d76b8bdf
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 01:04:19 +0530

    Create 6.1_Strings_Practice.js

---
+++ 6.1_Strings_Practice.js
@@ -0,0 +1,34 @@
+const name = "rajansrivastava"
+const age = 25
+const city = "New Delhi"
+
+console.log(`Hello my name is ${name} and my age is ${age} and I live in ${city}`);
+
+const surName = String("Srivastava")
+const surNames = new String("Srivastava")
+
+console.log(name.length);
+console.log(surName);
+console.log(surNames);
+
+console.log(surName.toUpperCase());
+console.log(surName.toLowerCase());
+console.log(surName.charAt(1));
+console.log(surName.indexOf('a'));
+
+
+const newString = surName.substring(0, 9)
+console.log(newString);
+
+
+const newStr = surName.slice(1, 5)
+console.log(newStr); // from 1 index to 4 index in slice(-1 from last no)
+ 
+
+
+
+
+
+
+
+
