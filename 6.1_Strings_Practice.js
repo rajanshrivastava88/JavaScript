caa5d15dd5d1ff6f12cd1ed81b5974741b1e170c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 01:18:50 +0530

    Update 6.1_Strings_Practice.js

--- 6.1_Strings_Practice.js
+++ 6.1_Strings_Practice.js
@@ -15,6 +15,8 @@ console.log(surName.toUpperCase());
 console.log(surName.toLowerCase());
 console.log(surName.charAt(1));
 console.log(surName.indexOf('a'));
+console.log(surName.split('-'));
+
 
 
 const newString = surName.substring(0, 9)
@@ -23,12 +25,43 @@ console.log(newString);
 
 const newStr = surName.slice(1, 5)
 console.log(newStr); // from 1 index to 4 index in slice(-1 from last no)
- 
 
 
 
+const str = "  Lalaji  "
+console.log(str);
+console.log(str.trim());
+
+
+const url = "https://rajan.com/rajan%20srivastava"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes("rajan"));
+console.log(url.includes("raja"));
+console.log(url.includes("najar"));
+
+
+let text = "HELLO WORLD";
+let letter1 = text.charAt(text.length-1);
+let letter2 = text.charAt(text.length-2);
+let letter3 = text.charAt(text.length-3);
+console.log(letter1, letter2, letter3);
 
 
 
+let txt = "Hello world!";
+let ans = text.repeat(2);   // Repeat Prototype
+console.log(ans);
+
+
+
+let text2 = "Hello world, welcome to the universe.";
+let result = text2.indexOf("welcome");    // Index of Prototype
+console.log(result);
+
+
+let text1 = "HELLO WORLD";
+let letter = text1.charAt(0);
+console.log(letter);
 
 
