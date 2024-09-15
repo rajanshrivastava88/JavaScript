ca768a02acf76aba615b4a83e53320fe1b721012
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 20:09:15 +0530

    Update 10_Array_Part-2.js

--- 10_Array_Part-2.js
+++ 10_Array_Part-2.js
@@ -1,6 +1,24 @@
 // Arrays Part-2
 
 
+const marvel_heros = ["Thor", "Ironman", "Spiderman"]
+const super_heros = ["Superman", "Batman", "Shaktiman"]
+
+//marvel_heros.push(super_heros)
+
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]); // 3rd index me 1st index => Batman
+
+
+const allHeros = marvel_heros.concat(super_heros)
+console.log(allHeros);
+
+
+const all_new_heros = [...marvel_heros, ...super_heros]
+console.log(all_new_heros);
+
+
+
 const fruits = new Array("Apple", "Mangos", "Grapes", "Bananas")
 const vegitables =  new Array("Potato", "Ladyfinger", "Culiflower", "Onion")
 
@@ -21,14 +39,15 @@ const defArray = abcArray.flat(3) // flat is used to Returns a new array and all
 console.log(defArray);
 
 
-console.log(Array.isArray(["Apple", "Banana"])); // its false because it is not an array its string
+console.log(Array.isArray("Apple", "Banana")); // false beacause it is not an array it is string
+console.log(Array.isArray(["Apple", "Banana"])); // its true because it is a array
 console.log(Array.from("Apple"));
-console.log(Array.from({name: "Apple"}));
+console.log(Array.from({name: "Apple"})); // interesting (empty array confuse between keys and values array)
 
 
 let marks1 = 150
 let marks2 = 200
-let marks3 = 250
+let marks3 = 230
 
 console.log(Array.of(marks1, marks2, marks3)); // of is used to convert all the variable values into array
 
