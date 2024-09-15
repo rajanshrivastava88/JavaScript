46699e3aa69d9245ed45f360adb193a39243147c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 21:24:00 +0530

    Update 13_Objects_Part-3.js

--- 13_Objects_Part-3.js
+++ 13_Objects_Part-3.js
@@ -1,22 +1,43 @@
+
+
 // Destructuring of Objects
 
 const course = {
-    course: "JavaScript",
+    coursename: "JavaScript",
     price: "1000",
     courseInstructor: "Rajan"
 }
 
-//course.courseInstructor
+// course.courseInstructor
+// course.coursename
+// course.price
 
-const {courseInstructor: instructor, price: Rate, course: js} = course
 
-// console.log(course.courseInstructor, instructor);
-// console.log(course.price, Rate);
-// console.log(course.course, js);
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+
+//const {courseInstructor: teacher} =  course
+//console.log(teacher);
+
+
+const {courseInstructor: instructor, price: Rate, course: js} = course // now courseInstructor known as instructor
+console.log(course.courseInstructor, instructor);
+console.log(course.price, Rate);
+console.log(course.course, js);
 
 //console.log(instructor, Rate, js);
 
-console.log(`My name is ${instructor} and im studing ${js} which price is ${Rate}`);
+//console.log(`My name is ${instructor} and im studing ${js} which price is ${Rate}`);
+
+
+// How Api's call works in json format
+
+// {
+//     "name": "rajan",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
 
 
 const student = {
