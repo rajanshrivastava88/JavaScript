5f02db49717fd9b94db6b02b10163b592c50c8cc
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 22:00:55 +0530

    Update 14_Functions.js

--- 14_Functions.js
+++ 14_Functions.js
@@ -1,6 +1,5 @@
 // Functions and Parameters
 
-
 // Functions ==> Function is a non-primitive datatype in which we can wrap-up a set of codes for a reusability.
 
 //console.log("R");
@@ -8,6 +7,16 @@
 //console.log("j");
 //console.log("a");
 //console.log("n");
+
+function sayMyName(){
+    console.log("A");
+    console.log("p");
+    console.log("p");
+    console.log("l");
+    console.log("e");
+}
+
+//sayMyName()  // Calling the function
 
 function myName(){
     console.log("R");
@@ -21,10 +30,100 @@ function myName(){
 
 // Addition of Two Numbers in Function
 
+
+/*
+
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);   
+}
+
+//addTwoNumbers()   // o/p => NaN
+// addTwoNumbers(8, 9)
+// addTwoNumbers(8, "9") // javascripts merge so it become 89
+// addTwoNumbers(8, "a")
+// addTwoNumbers(8, null)
+
+const result = addTwoNumbers(9, 6)
+console.log("Result: ", result); 
+
+Ex :-
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2; 
+    console.log("Rajan")
+    return result  
+    // after return if we write any code it will not be worked
+}
+
+
+*/
+
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2; 
+    // return result  
+    // after return if we write any code it will not be worked
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5) // the return value can be stored into variable not only console.log
+//console.log("Result: ", result);
+
+
+
+
+/*
+
+function loginUserMessage(username){
+    if(username === undefined){ // it will run always in true situation
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("rajan"))
+//console.log(loginUserMessage(""))
+console.log(loginUserMessage()) // undefined
+
+*/
+
+function loginUserMessage(username){
+    if(!username){ 
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("rajan"))
+console.log(loginUserMessage()) 
+
+
+
+
+function loginUserMessage(username = "sam"){ // sam is by default value
+    if(!username){ 
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("rajan"))
+console.log(loginUserMessage())
+//console.log(loginUserMessage("Lalaji"))
+
+
+
+/*
+
+
 function additionOfTwoNo(num1, num2) {   // additionOfTwoNo (num1, num2)=> num1 & num2 is known as Parameters
     //console.log(num1 + num2);
     //let total= num1+num2
-    return num1+num2  // retuen total
+    return num1+num2  // return total
 }
 //additionOfTwoNo(8, 88) // additionOfTwoNo(8, 88) => it is known as Arguments
 //console.log(additionOfTwoNo(8, 88)); //96 undefined 
@@ -47,8 +146,6 @@ function loginUserMessage(username = "sam") {
                                                     // inside the arguments then the new value will be override
 
 
-
-
 function additionOfTwoNumbers() {
     let a = 10
     let b = 20
@@ -58,4 +155,4 @@ function additionOfTwoNumbers() {
 }
 additionOfTwoNumbers()
 
-
+*/
