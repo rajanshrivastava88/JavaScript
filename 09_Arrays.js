076fd21da73a8abe322b4fea52ddc049bb544b9f
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 16:24:02 +0530

    Update 09_Arrays.js

--- 09_Arrays.js
+++ 09_Arrays.js
@@ -5,19 +5,27 @@
 const myArray = [0, 1, 2, 6, 8, "Rajan", "Tushar", 9.5 ]
 console.log(myArray[7]);
 
-const Arr = ["Shaktiman", "Baalveer", "Soanpari"]
-//console.log(Arr[0])
+const myHeros = ["Shaktiman", "Baalveer", "Soanpari"]
+console.log(myHeros[0])
 
-const myArray1 = new Array(1, 2, 3, 4, 5, 6, 7, 8)
-console.log(myArray1[5]);
+const myArray2 = new Array(1, 2, 3, 4, 5, 6, 7, 8)
+console.log(myArray2[5]);
+
+
 
 // Array Methods
 
-myArray.push(10)
-//console.log(myArray);
+myArray.push(10)  // push() method is used to add elements in the array from the back side
+myArray.push(15) 
+console.log(myArray);
 
 myArray.pop()  // pop() use as a method and we can't write anything inside the pop
-//console.log(myArray); // pop remove the number from the last
+console.log(myArray); // pop remove the number from the last
+
+
+
+
+// Shift and Unshift Method
 
 
 myArray.unshift(9)  // unshift add the no from starting of the array
@@ -27,20 +35,24 @@ myArray.shift()  // shift remove the number from starting of the array
 //console.log(myArray);
 
 
-const myNewArr = new Array(15,20,31,42,53,65,7,80,9,0)
-//console.log(myNewArr.includes(10));  // It shows the particular no is available or not in the array so the o/p =>false
+// includes() method shows the that number or string is available or not in that array
 
-//console.log(myNewArr.indexOf(8)); // 8 kaun se index pe hai
+
+const myNewArr = new Array(15,20,31,42,53,65,7,80,9,0)
+console.log(myNewArr.includes(10));  // It shows the particular no is available or not in the array so the o/p =>false
+
+console.log(myNewArr.indexOf(8)); // 8 kaun se index pe hai
 
 const newArr = myNewArr.join('') // join is used to join all the elements together to form a single string
-// console.log(newArr); // if we use paranthesis then the bydefault the elements will be displayed with commas
-// console.log(typeof newArr); // if we use join('') then there will be no comma between the elements
+console.log(newArr); // if we use paranthesis then the bydefault the elements will be displayed with commas
+console.log(typeof newArr); // if we use join('') then there will be no comma between the elements
 
 
 
 // Slice and Splice
 
-//console.log("A","B", myNewArr);
+
+console.log("A","B", myNewArr);
 
 const myn1 = myNewArr.slice(0, 5) //Slice the indexing of 0 to 5 the 5th indexing will not be printed and 0 to 4 will be printed
 
