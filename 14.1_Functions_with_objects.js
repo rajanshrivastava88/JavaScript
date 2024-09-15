d9a8e9319795947c8e17785eabc5d77a7c14e4b0
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-15 22:11:19 +0530

    Update 14.1_Functions_with_objects.js

--- 14.1_Functions_with_objects.js
+++ 14.1_Functions_with_objects.js
@@ -1,12 +1,25 @@
 // Functions with Objects
 
 
-function calculateCartPrice(var1, var2, var3, ...num1){  // function calculateCartPrice(...num1)  ==> rest operator
+function calculateCartPrice(...num1){
     return num1
 }
 
-//console.log(calculateCartPrice(100, 200, 300, 800, 200000));
+//console.log(calculateCartPrice(200, 400, 500, 2000));
 
+
+
+
+
+function calculateCartPrice1(val1, val2, val3, ...num1){  // function calculateCartPrice(...num1)  ==> rest operator/Spread Operator
+    return num1
+}
+
+//console.log(calculateCartPrice1(100, 200, 300, 800, 200000));
+
+
+
+// Object passes into Function
 
 const user = {
     username: "rajan",
@@ -17,17 +30,20 @@ function handleObject(anyobject) {
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
-//handleObject(user)
+//handleObject(user) calling the function
 handleObject ({
     username: "raj",
     price: 499
 })
 
+
+// Passing the arrays
+
 const myNewArray = [200, 400, 800, 1000]
 
 function returnSecondValue(getArray){
-    return getArray[2]
+    return getArray[1]
 }
 
-// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([100, 300, 400, 800]));
