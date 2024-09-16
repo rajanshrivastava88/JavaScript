26699fd79b85c0aee3d66d0642142c8049adde17
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-16 11:46:14 +0530

    Create 15_Global_and_Localscope.js

---
+++ 15_Global_and_Localscope.js
@@ -0,0 +1,54 @@
+// Global and Local Scope
+
+// let b = 40
+// const c = 50
+// var d = 60
+
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+var f = 88
+
+if (true) {
+    let b = 40
+    const c = 50
+    //var d = 60
+    e = 70
+}
+
+//console.log(b);
+//console.log(c);
+console.log(d);
+console.log(e);
+
+
+
+
+/*
+
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    //console.log("INNER: ", a);
+
+}
+
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c); 
+
+a = 500
+if (true) {
+    let a = 80
+    const c = 90
+    console.log("Inner :", a, c);
+}
+
+*/
