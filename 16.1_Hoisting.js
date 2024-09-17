b132553a0f45a7861fc3cfd37d7bfdc1e0795c3b
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-17 21:21:11 +0530

    Update 16.1_Hoisting.js

--- 16.1_Hoisting.js
+++ 16.1_Hoisting.js
@@ -1,6 +1,12 @@
 
+/*
 
-//  Hoisting
+ Hoisting : JavaScript scope and hoisting are fundamental concepts that every developer should understand 
+            to write efficient and error-free code. These concepts determine how variables and functions are 
+            accessed and executed within a JavaScript program.
+
+
+*/
 
 function hoist(){
     console.log(a);
