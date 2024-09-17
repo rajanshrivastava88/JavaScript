9d5a2ab39bfc0ecd363778f31acc32764e45d5a9
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-17 22:08:40 +0530

    Update 16.1_Hoisting.js

--- 16.1_Hoisting.js
+++ 16.1_Hoisting.js
@@ -6,6 +6,22 @@
             accessed and executed within a JavaScript program.
 
 
+Introduction to Scope in JavaScript.
+
+Scope in JavaScript is a concept that defines the accessibility of variables, functions, and objects in 
+some particular part of your code during runtime. Scope can be thought of as a set of rules that determine 
+where a variable or function is accessible.
+
+
+What is Scope?
+
+Scope determines the visibility and lifetime of variables and functions. 
+JavaScript primarily has two types of scope: global and local.
+
+Global Scope: Variables declared outside of any function or block are in the global scope. They can be accessed from anywhere in the code. Global scope is the outermost scope.
+Local Scope: Variables declared within a function or block are in the local scope. They can only be accessed within that specific function or block.
+
+
 */
 
 function hoist(){
