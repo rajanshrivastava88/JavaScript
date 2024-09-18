3d483736d75139810c6c75cb122ed9802ff248f0
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-18 19:18:32 +0530

    Create 20_Control_Flow.js

---
+++ 20_Control_Flow.js
@@ -0,0 +1,80 @@
+// if statement
+
+//const isUserloggedIn = true
+const temperature = 35
+
+if ( temperature === 40 ){
+    //console.log("less than 50");
+}
+else 
+{
+   //console.log("temperature is greater than 50");
+}
+
+//console.log("Execute");
+
+
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+const score = 500
+
+if (score > 100) {
+    //var power = "fly"
+    //console.log("Print True value");  //console.log(`User power: ${power}`);
+}
+
+//console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+if (balance < 500) 
+//console.log("test")
+//else
+//console.log("test2");
+
+if (balance < 500) 
+{
+    // console.log("less than 500");
+} 
+else if (balance < 750) 
+{
+    // console.log("less than 750");
+    
+} 
+else if (balance < 900) 
+{
+    // console.log("less than 750");
+    
+} 
+else 
+{
+    // console.log("less than 1200");
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && loggedInFromGoogle && loggedInFromEmail) { // all conditions should be true then if condition will be run otherwise else condition will run if any condition will be false
+    //console.log("Allow to buy course");
+}
+else 
+{
+    //console.log("Don't allow to buy the course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    //console.log("User logged in");
+}
+
+
+
+const Month = 12
+
+if(Month == 12)
+{
+    console.log("January");
+}
