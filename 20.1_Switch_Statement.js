9809d7f5036663073c656fa14330a82c7c84c15d
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-18 20:29:08 +0530

    Create 20.1_Switch_Statement.js

---
+++ 20.1_Switch_Statement.js
@@ -0,0 +1,86 @@
+// Switch statement
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+if (!~0)
+    console.log("Executed");
+    else
+    console.log("Not executed");
+    
+    
+    
+    const month = "march"
+    
+    switch (month) {
+        case "jan":
+            console.log("January");
+            break;
+        case "feb":
+            console.log("feb");
+            break;
+        case "march":
+            console.log("march");
+            break;
+        case "april":
+            console.log("april");
+            break;
+    
+        default:
+            console.log("default case match");
+            break;
+    }
+    
+    
+    const month1 = 8
+    
+    switch (month1) {
+        case 1:
+            console.log("January");
+            break;
+        case 2:
+            console.log("feb");
+            break;
+        case 3:
+            console.log("march");
+            break;
+        case 4:
+            console.log("april");
+            break;
+        case 5:
+            console.log("may");
+            break;
+        case 6:
+            console.log("june");
+            break;
+        case 7:
+            console.log("july");
+            break;
+        case 8:
+            console.log("august");
+            break;
+        case 9:
+            console.log("september");
+            break;
+        case 10:
+            console.log("october");
+            break; 
+        case 11:
+            console.log("november");
+            break; 
+        case 12:
+            console.log("december");
+            break;
+        
+    
+        default:
+            console.log("default case match");
+            break;
+    }
