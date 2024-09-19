e6c56efe38c16ce85c04a84d0ce960a5da50f16d
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-19 20:07:09 +0530

    Create 22_While_and_Do_While_Loop.js

---
+++ 22_While_and_Do_While_Loop.js
@@ -0,0 +1,67 @@
+// While and Do while loop
+
+/*
+While loop
+
+1. A while loop evaluates the condition inside the parenthesis ().
+2. If the condition evaluates to true, the code inside the while loop is executed.
+3. The condition is evaluated again.
+4. This process continues until the condition is false.
+5. When the condition evaluates to false, the loop stops.
+
+*/
+
+// program to display numbers from 1 to 5
+// initialize the variable
+
+
+let i = 1, n = 5;
+
+// while loop from i = 1 to 5
+while (i <= n) {
+    console.log(i);
+    i += 1;
+}
+
+
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ['Mahadev Ji', "Maa Parvati Ji", "Shri Ganesh ji", "Maa Sarswati ji", 
+"Maa Laxmi ji", "Kumar Kartikey ji", "Savi Devi ji Aur Devta Gan ji"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+
+/*
+The Do While Loop
+The do while loop is a variant of the while loop. This loop will execute the code block once, 
+before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+
+*/
+
+let text = ""
+let j = 0;
+
+do {
+  text += "The number is " + j;
+  j++;
+}
+while (j < 10); 
+
+console.log(j);
