05412cb390e04335b1b4cfed02d36c85fe630140
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-20 17:49:15 +0530

    Update 26.3_Reduce().js

--- 26.3_Reduce().js
+++ 26.3_Reduce().js
@@ -14,3 +14,25 @@ const sumWithInitial = array1.reduce(
 
 console.log(sumWithInitial);
 // Expected output: 10
+
+
+
+const myNum = [1, 2, 3]
+
+const myTotals = myNum.reduce( function (acc, currentvalue) {
+    return acc + currentvalue
+}, 0)
+
+console.log(myTotals);
+
+
+
+const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce( function (acc, currentvalue) {
+    console.log(`acc: ${acc} and currentvalue: ${currentvalue}`);
+    
+    return acc + currentvalue
+}, 3)
+
+console.log(myTotal);
