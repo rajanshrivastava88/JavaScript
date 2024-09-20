68555348eb35a6b2cfcd1b86900980c2991905a7
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-20 15:42:02 +0530

    Update 26_Filter Map_and_Reduce.js

--- 26_Filter Map_and_Reduce.js
+++ 26_Filter Map_and_Reduce.js
@@ -14,15 +14,35 @@ console.log(values); // forEach does not return any Values
 
 const myNums = [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNums.filter( (num) => num > 4)
-console.log(newNums);
+// const newNums = myNums.filter( (num) => num > 4)
+// console.log(newNums);
 
 // or
 
 
 const myNums1 = [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums1 = myNums1.filter( (num) =>{
-    return num > 4
+const newNums1 = myNums1.filter( (num) =>{  
+    return num > 4   // if we will open scope then we have to write return keyword  
 })
 console.log(newNums1);
+
+
+
+const newNums = []
+
+myNums.forEach( (num) => {
+    if (num > 4) {
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
+
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004},
+    { title: 'Book Two', genre: 'Fiction', publish: 1981, edition: 2004},
+
+]
