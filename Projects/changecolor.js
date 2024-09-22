456309244b0772096ae699e11459b676f84769eb
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-22 19:12:20 +0530

    Update changecolor.js

--- Projects/changecolor.js
+++ Projects/changecolor.js
@@ -1,11 +1,11 @@
-const buttons = document.querySelectorAll(".button")
+const buttons = document.querySelectorAll(".buttons")
 //console.log(buttons);
 const body = document.querySelector("body")
 
 
-buttons.forEach(function (button){
-    console.log(button);
-    button.addEventListener('click', function(changeColor){
+buttons.forEach(function (buttons){
+    console.log(buttons);
+    buttons.addEventListener('click', function(changeColor){
         console.log(changeColor);
         console.log(changeColor.target);
         if (changeColor.target.id === 'grey') {
