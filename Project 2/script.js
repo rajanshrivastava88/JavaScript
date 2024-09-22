ff9071ea97ddff70ec384b5571a1f02e70464a0a
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-23 01:28:28 +0530

    Update script.js

--- Project 2/script.js
+++ Project 2/script.js
@@ -13,3 +13,10 @@ searchBtn.addEventListener("click", () => {
 
 
 // The $& is a special replacement pattern that represents the matched substring.
+
+// The resulting HTML will have the matched text wrapped within <mark> tags, 
+// which can be styled using CSS to highlight the matches.
+
+
+// replace(regExp, "<mark>$&</mark>"): Replaces all occurrences of the regular expression 
+// regExp within the paragraph's text with the string <mark>$&</mark>. 
