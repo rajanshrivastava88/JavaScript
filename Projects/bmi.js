9ea8c8d58ff41492db915b2c77c32cbc4fd26faa
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-24 22:35:20 +0530

    Update bmi.js

--- Projects/bmi.js
+++ Projects/bmi.js
@@ -1,2 +1,11 @@
-const bmiText = document.getElementById('bmi')
-const descText = document.getElementById('desc')
+const bmiText = document.getElementById('bmi');
+const descText = document.getElementById('desc');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    const weight = parseFloat
+}
