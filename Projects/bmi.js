bad7578a3e9dd83da2b5dc0d572b7e13e0c2c28d
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-24 22:42:22 +0530

    Update bmi.js

--- Projects/bmi.js
+++ Projects/bmi.js
@@ -22,4 +22,23 @@ function onFormReset() {
         return;
       }
     
-     
+      const heightInMeters = height / 100; // cm -> m
+      const bmi = weight / Math.pow(heightInMeters, 2);
+      const desc = interpretBMI(bmi);
+    
+      bmiText.textContent = bmi.toFixed(2);
+      bmiText.className = desc;
+      descText.innerHTML = `You are <strong>${desc}</strong>`;
+    }
+    
+    function interpretBMI(bmi) {
+      if (bmi < 18.5) {
+        return "underweight";
+      } else if (bmi < 25) {
+        return "healthy";
+      } else if (bmi < 30) {
+        return "overweight";
+      } else {
+        return "obese";
+      }
+    }
