0acc7904d72b65e77b4713e948d4c973faaf44d9
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-25 22:08:22 +0530

    Update bmiscript.js

--- Projects/bmiscript.js
+++ Projects/bmiscript.js
@@ -12,5 +12,12 @@ form.addEventListener('submit',function (e){
     if (height === '' || height < 0 || isNaN(height)) {
         results.innerHTML = `Please give a valid height ${height}`;
     }
-    results.innerHTML = `${height}`;
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid weight ${weight}`;
+    } else {
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
+        //show the result
+        results.innerHTML = `<span>${bmi}</span`;
+    }
+    
 });
