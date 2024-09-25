e0ed72e0e9d3ae26faa20c25457f3b10e4ef0b34
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-25 22:00:21 +0530

    Update bmiscript.js

--- Projects/bmiscript.js
+++ Projects/bmiscript.js
@@ -1,8 +1,16 @@
 const form = document.querySelector('form')
+// This usecase will give you empty
+const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit',function (e){
     e.preventDefault()
 
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
-})
+    const results = document.querySelector('#results')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`;
+    }
+    results.innerHTML = `${height}`;
+});
