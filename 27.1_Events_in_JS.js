699ee4e0f81837527ad8c9e911e80cd30aa034fc
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-27 02:49:32 +0530

    Create 27.1_Events_in_JS.js

---
+++ 27.1_Events_in_JS.js
@@ -0,0 +1,29 @@
+/*
+
+Events in JavaScript
+
+Events are things that happen in the system you are programming, which the system tells you about so 
+your code can react to them.
+
+For example, if the user clicks a button on a webpage, you might want to react to that action by 
+displaying an information box. 
+
+There are many different types of events that can occur.
+
+For example:
+
+1. The user selects, clicks, or hovers the cursor over a certain element.
+
+2. The user chooses a key on the keyboard.
+
+3. The user resizes or closes the browser window.
+
+4. A web page finishes loading.
+
+5. A form is submitted.
+
+6. A video is played, paused, or ends.
+
+7. An error occurs.
+
+*/
