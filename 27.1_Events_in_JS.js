0edfa045587f1c7a46a94948e461b152e9b6a7b9
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-27 02:54:20 +0530

    Update 27.1_Events_in_JS.js

--- 27.1_Events_in_JS.js
+++ 27.1_Events_in_JS.js
@@ -26,4 +26,30 @@ For example:
 
 7. An error occurs.
 
+
+
+Common JavaScript Events Table
+
+Event Attribute	                            Description
+
+onclick	                        Triggered when an element is clicked.
+
+onmouseover	                    Fired when the mouse pointer moves over an element.
+
+onmouseout	                    Occurs when the mouse pointer leaves an element.
+
+onkeydown	                    Fired when a key is pressed down.
+
+onkeyup                        	Fired when a key is released.
+
+onchange	                    Triggered when the value of an input element changes.
+
+onload	                        Occurs when a page has finished loading.
+
+onsubmit	                    Fired when a form is submitted.
+
+onfocus	                        Occurs when an element gets focus.
+
+onblur	                        Fired when an element loses focus.
+
 */
