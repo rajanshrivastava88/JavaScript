ed25b97bd031a2bb76e510b38b910cfb202d6e36
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-28 00:54:47 +0530

    Update 28_Async_JS_Fundamentals.js

---
+++ 28_Async_JS_Fundamentals.js
@@ -0,0 +1,15 @@
+/*
+Async Code in JavaScript
+
+Async Function
+
+The async function allows us to write promise-based code as if it were synchronous. 
+This ensures that the execution thread is not blocked.
+
+Promise Handling: Async functions always return a promise. If a value is returned that is not a promise, 
+JavaScript automatically wraps it in a resolved promise.
+
+
+
+
+*/
