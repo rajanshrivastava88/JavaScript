36e1f56eaca822f0bc951caeef804880516191f3
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-28 22:53:21 +0530

    Update script.js

---
+++ Random Color Generate/script.js
@@ -0,0 +1,9 @@
+// Generate a random color
+
+const randomColor = function() {
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {}
+};
+
+console.log(Math.random() * 16);
