3323d7c3dff3c8cd82e56f1ef44c31c2296f6d45
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-30 15:20:50 +0530

    Update script.js

--- (Async JS Project) Random Color Generator/script.js
+++ (Async JS Project) Random Color Generator/script.js
@@ -1,5 +1,4 @@
 // Generate a random color
-
 const randomColor = function() {
     const hex = '0123456789ABCDEF';
     let color = '#';
@@ -9,4 +8,22 @@ const randomColor = function() {
     return color;
 };
 
+let intervalId;
+const startChangingColor = function() {
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColor, 1000);
+    }
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+}
+};
+
+const stopChangingColor = function() {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
 document.querySelector("#start").addEventListener('click', startChangingColor)
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
