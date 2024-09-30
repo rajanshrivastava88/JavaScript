f942ae436b4694dc3bbc8673f091fb163d017ef6
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-09-30 10:18:05 +0530

    Update 26.3_Reduce().js

--- 26.3_Reduce().js
+++ 26.3_Reduce().js
@@ -1,6 +1,19 @@
 
 // Reduce () Method 
 
+/*
+
+The reduce() method executes a reducer function for array element.
+
+The reduce() method returns a single value: the function's accumulated result.
+
+The reduce() method does not execute the function for empty array elements.
+
+The reduce() method does not change the original array.
+
+
+*/
+
 // Mostly used in Shopping cart
 
 // Eg:-
