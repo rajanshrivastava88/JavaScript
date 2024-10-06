9a40dd7a266996d3de16bf96971b7e9698fa60c5
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-06 17:35:10 +0530

    Update 10.1_Arrays_Pactice.js

--- 10.1_Arrays_Pactice.js
+++ 10.1_Arrays_Pactice.js
@@ -14,6 +14,7 @@ If (" ") is used as separator, the string is split between words.
 
 */
 
+// Split the Words
 
 let text = "How are you doing today?";
 const myArray = text.split(" ");
@@ -21,10 +22,22 @@ const myArray = text.split(" ");
 console.log(myArray);
 
 
+// Split the words, and return the second word
 
 let text1 = "How are you doing today?";
 const myArray1 = text.split(" ");
 let word = myArray1[1];
 
 console.log(word);
+
+// Split the characters, including spaces:
+
+const myArray2 = text1.split("");
+console.log(myArray2);
+
+
+// Use the limit parameter
+
+const myArray3 = text1.split("", 3) // (" ",3)
+console.log(myArray3);
 
