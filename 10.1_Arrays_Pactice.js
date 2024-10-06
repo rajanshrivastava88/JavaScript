fdd1de70f082c65f1fe7fbb03b0375a465016e47
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-06 17:26:35 +0530

    Create 10.1_Arrays_Pactice.js

---
+++ 10.1_Arrays_Pactice.js
@@ -0,0 +1,30 @@
+
+// Split () Method
+
+/*
+
+The split() method splits a string into an array of substrings.
+
+The split() method returns the new array.
+
+The split() method does not change the original string.
+
+If (" ") is used as separator, the string is split between words.
+
+
+*/
+
+
+let text = "How are you doing today?";
+const myArray = text.split(" ");
+
+console.log(myArray);
+
+
+
+let text1 = "How are you doing today?";
+const myArray1 = text.split(" ");
+let word = myArray1[1];
+
+console.log(word);
+
