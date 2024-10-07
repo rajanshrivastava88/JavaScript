e87f27f31790de3cc0a5f9687755e03680450707
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-07 18:02:28 +0530

    Update 10.1_Arrays_Pactice.js

--- 10.1_Arrays_Pactice.js
+++ 10.1_Arrays_Pactice.js
@@ -14,18 +14,59 @@ If (" ") is used as separator, the string is split between words.
 
 */
 
+const cities = ["Tokyo","New York","Los Angeles","Paris","Seattle"];
+
+const newCityArr = cities.slice(2);
+
+console.log("Original: ", cities)
+console.log("New: ", newCityArr)
+
+const newCity = cities.slice(-1)
+console.log(newCity);
+
+
+const newCities = cities.slice(2, -1)
+console.log(newCities);
+
+
+// splice() JavaScript array method
+
+/*
+The splice() method will change the contents of the original array. The splice() method is used to add or remove elements of 
+an existing array and the return value will be the removed items from the array.
+
+
+
+If nothing was removed from the array, then the return value will just be an empty array.
+
+Here is the basic syntax.
+
+splice(start, optional delete count, optional items to add)
+
+*/
+
+const food = ['Pizza', 'Burger', 'Maggi', 'Badapav', 'Chawmin']
+
+const lunch = food.splice(3)
+
+console.log(lunch);
+
+
+
+
+
 // Split the Words
 
-let text = "How are you doing today?";
-const myArray = text.split(" ");
+// let texts = "How are you doing today?";
+// const myArray = texst.split(" ");
 
-console.log(myArray);
+// console.log(myArray);
 
 
 // Split the words, and return the second word
 
 let text1 = "How are you doing today?";
-const myArray1 = text.split(" ");
+const myArray1 = text1.split(" ");
 let word = myArray1[1];
 
 console.log(word);
@@ -55,14 +96,14 @@ console.log(myArrays[1]);
 
 // Use a letter as a separator:
 
-const myArr = text1.split("o");
-console.log(myArr);
+// const myArr = text1.split("o");
+// console.log(myArr);
 
 
 // If the separator parameter is omitted, an array with the original string is returned:
 
-const myArrays = text.split();
-console.log(myArrays);
+// const myArrays = texts.split();
+// console.log(myArrays);
 
 
 
