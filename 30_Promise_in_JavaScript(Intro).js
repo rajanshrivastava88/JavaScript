07b43840ef471dac7ef0f7e2a6d53fe333a330fc
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-09 22:38:41 +0530

    Update 30_Promise_in_JavaScript(Intro).js

--- 30_Promise_in_JavaScript(Intro).js
+++ 30_Promise_in_JavaScript(Intro).js
@@ -206,5 +206,24 @@ function getFile(myCallback) {
 getFile(myDisplayer);
 
 
+Example using Promise
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let req = new XMLHttpRequest();
+  req.open('GET', "mycar.html");
+  req.onload = function() {
+    if (req.status == 200) {
+      myResolve(req.response);
+    } else {
+      myReject("File not Found");
+    }
+  };
+  req.send();
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+
 
 */
