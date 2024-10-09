b52e8b1ff2ad0fda896a0a83031b7b16be918d61
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-09 21:01:20 +0530

    Update 30_Promise_in_JavaScript(Intro).js

--- 30_Promise_in_JavaScript(Intro).js
+++ 30_Promise_in_JavaScript(Intro).js
@@ -28,3 +28,40 @@ Promise :- The Promise object represents the eventual completion (or failure) of
            attached.
 
 */
+
+
+/*
+
+JavaScript Promises
+
+
+                    "I Promise a Result!"
+
+            "Producing code" is code that can take some time
+
+            "Consuming code" is code that must wait for the result
+
+            A Promise is an Object that links Producing code and Consuming code
+
+
+JavaScript Promise Object :
+
+A Promise contains both the producing code and calls to the consuming code:
+
+
+Promise Syntax: 
+
+let myPromise = new Promise(function(myResolve, myReject) {
+// "Producing Code" (May take some time)
+
+  myResolve(); // when successful
+  myReject();  // when error
+});
+
+// "Consuming Code" (Must wait for a fulfilled Promise)
+myPromise.then(
+  function(value) { /* code if successful */ },
+  function(error) { /* code if some error */ }
+);
+
+*/
