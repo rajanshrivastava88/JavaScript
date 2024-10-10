bee09dc810a89574998b46f21a21d99a06c27784
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-10 21:39:34 +0530

    Update 30.1_Promise.js

--- 30.1_Promise.js
+++ 30.1_Promise.js
@@ -4,7 +4,23 @@ const promiseOne = new Promise(function(resolve, reject){
     // DB calls, cryptography, network
     setTimeout(function(){
         console.log('Async task is complete');
+        resolve()
         
     }, 1000)
 })
 
+promiseOne.then(function(){
+    console.log("Promise Consumed");
+    
+})
+
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    }, 1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+    
+})
