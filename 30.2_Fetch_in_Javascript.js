a21805097150ad50cfab4655a42f0d86d60f4549
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-12 19:58:14 +0530

    Update 30.2_Fetch_in_Javascript.js

---
+++ 30.2_Fetch_in_Javascript.js
@@ -0,0 +1,12 @@
+
+// getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error);
+)
