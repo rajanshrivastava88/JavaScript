c827d9a7d3d71f76f023b0b9dc98e5624cce94b7
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-13 21:39:13 +0530

    Create 34_Prototype.js

---
+++ Classes & OOP/34_Prototype.js
@@ -0,0 +1,27 @@
+
+// Prototype
+
+let myName = "rajan    "
+let myChannel = "chai     "
+
+// console.log(myName.trim().length);
+
+// console.log(myName.length);
+
+// console.log(myName.truelength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function() { // method -> function inject
+        console.log(`Spidy power is ${this.spiderman}`);  
+    }
+}
+
+bject
+
+heroPower.rajan()
