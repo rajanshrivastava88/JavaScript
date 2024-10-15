ba34f4a462e21ed232eb91651fea319138713dd7
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-15 22:00:55 +0530

    Update 36_Call.js

--- Classes & OOP/36_Call.js
+++ Classes & OOP/36_Call.js
@@ -1,5 +1,9 @@
-// call
+/* call Method
 
+The call() method of Function instances calls this function with a given this value and arguments provided individually.
+
+
+*/
 function SetUsername(username) {
     //complex DB calls
     this.username = username
@@ -16,3 +20,19 @@ function createUser(username, email, password){
 
 const evening = new createUser("chai", "chai@fb.com", "123")
 console.log(evening);
+
+
+
+
+function Product(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  
+  function Food(name, price) {
+    Product.call(this, name, price);
+    this.category = 'food';
+  }
+  
+  console.log(new Food('cheese', 5).name);
+  // Expected output: "cheese"
