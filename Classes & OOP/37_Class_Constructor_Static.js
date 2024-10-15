244799e2adae5b4624073270bda7f08179883d2c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-15 22:44:15 +0530

    Update 37_Class_Constructor_Static.js

--- Classes & OOP/37_Class_Constructor_Static.js
+++ Classes & OOP/37_Class_Constructor_Static.js
@@ -3,6 +3,12 @@
 
 class User {
     constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password
+    }
+
+    encryptPassword() {
         
     }
 }
