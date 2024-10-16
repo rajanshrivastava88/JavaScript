8684ccd177282093a7f1632ba14cd7278a6e136c
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-16 21:11:30 +0530

    Update 38_Static_Properties.js

--- Classes & OOP/38_Static_Properties.js
+++ Classes & OOP/38_Static_Properties.js
@@ -1,7 +1,29 @@
 // Static Properties
 
-class USer {
+class User {
     constructor(username) {
         this.username = username
     }
+
+    logMe() {
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId() {
+        return`9988`
+    }
 }
+
+const rajan = new User("rajan")
+//console.log(rajan.createId());
+
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
