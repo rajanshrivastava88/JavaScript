dc9bb779127e2c69ebee3f1ceb1a549968a68c28
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-16 21:13:06 +0530

    Update 38_Static_Properties.js

--- Classes & OOP/38_Static_Properties.js
+++ Classes & OOP/38_Static_Properties.js
@@ -1,5 +1,19 @@
 // Static Properties
 
+
+/*
+
+The static keyword defines a static method or field for a class, or a static initialization block 
+(see the link for more information about this usage). Static properties cannot be directly accessed 
+on instances of the class. Instead, they're accessed on the class itself.
+
+Static methods are often utility functions, such as functions to create or clone objects, whereas 
+static properties are useful for caches, fixed-configuration, or any other data you don't need to be 
+replicated across instances.
+
+
+*/
+
 class User {
     constructor(username) {
         this.username = username
