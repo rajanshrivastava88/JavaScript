4357edc0eaee05b08d330bea146d4605db6429b8
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-16 20:25:08 +0530

    Update 35.1_Inheritance.js

---
+++ Classes & OOP/35.1_Inheritance.js
@@ -0,0 +1,20 @@
+// Inheritance
+
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe() {
+        console.log(`USERNAME is ${this.username}`);
+        
+    }
+}
+
+class Teacher extends User {
+    constructor(username, email, password) {
+        super(username)
+        this.email = email
+        this.password = password
+    }
+}
