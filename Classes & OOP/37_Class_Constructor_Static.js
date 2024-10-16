cae3c104dbe11ea6d39af53368f7e314119a642a
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-16 19:49:31 +0530

    Update 37_Class_Constructor_Static.js

--- Classes & OOP/37_Class_Constructor_Static.js
+++ Classes & OOP/37_Class_Constructor_Static.js
@@ -1,24 +1,48 @@
 
 // ES6
 
-class User {
-    constructor(username, email, password){
-        this.username = username;
-        this.email = email;
-        this.password = password
-    }
+// class User {
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password
+//     }
 
-    encryptPassword() {
-        return `${this.password}abc`
-    }
-    changeUsername(){
-        return `${this.username.toUpperCase()}`
-    }
+//     encryptPassword() {
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
 
+// }
+
+// const evening = new User("Chai", "Chai@gmail.com", "123")
+// console.log(evening.encryptPassword());
+
+// console.log(evening.changeUsername());
+
+
+
+
+// Behind the Scene
+
+function User (username, email, password) {
+    this.username = username
+    this.email = email
+    this.password = password
 }
 
-const evening = new User("Chai", "Chai@gmail.com", "123")
-console.log(evening.encryptPassword());
+User.prototype.encryptPassword = function() {
+    return `${this.password}abc`
+}
 
-console.log(evening.changeUsername());
+User.prototype.changeUsername = function() {
+    return `${this.username.toUpperCase()}`
+}
+
+const name = new User("rajan", "rajan@gmail.com",  "8899")
+
+console.log(name.encryptPassword());
+console.log(name.changeUsername());
 
