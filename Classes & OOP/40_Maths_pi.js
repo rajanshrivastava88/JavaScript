4cd92b1b53458eb6835ba53638cc658fd43766cc
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-17 16:40:38 +0530

    Update 40_Maths_pi.js

--- Classes & OOP/40_Maths_pi.js
+++ Classes & OOP/40_Maths_pi.js
@@ -3,10 +3,9 @@
 
 // Object.getOwnPropertyDescriptor(Math)
 
-const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
 
-console.log(descriptor);
-
+// console.log(descripter);
 
 // console.log(Math.PI);
 // Math.PI = 5
@@ -15,22 +14,26 @@ console.log(descriptor);
 const chai = {
     name: 'ginger chai',
     price: 250,
-    isAvailable: true
+    isAvailable: true,
 
-    orderchai
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
 }
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-Object.defineProperties(chai, 'name', {
-    writable: false,
-    enumerable: false
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
 })
 
 console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-
-for (let [key, value] of chai) {
-    console.log(`${key} : ${value}`);
-    
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
 }
