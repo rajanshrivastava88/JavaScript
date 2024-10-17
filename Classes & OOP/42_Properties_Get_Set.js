e2944b493503c8c2ef753f96e1c4a9bb91934b7f
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2024-10-17 17:52:57 +0530

    Update 42_Properties_Get_Set.js

--- Classes & OOP/42_Properties_Get_Set.js
+++ Classes & OOP/42_Properties_Get_Set.js
@@ -1,10 +1,28 @@
 // Properties Get & Set
 
-function User(email, password) {
+function User(email, password){
     this._email = email;
     this._password = password
 
-    Object.defineProperties(this, 'email', {
-        
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
     })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
 }
+
+const breakfast = new User("chai@chai.com", "chai")
+
+console.log(breakfast.email);
