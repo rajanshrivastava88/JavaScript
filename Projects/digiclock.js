2f7a9f95197d97dc1220c0f32b5a94fe3b5d0805
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-07 20:30:25 +0530

    Update digiclock.js

--- Projects/digiclock.js
+++ Projects/digiclock.js
@@ -1,6 +1,6 @@
 const clock = document.getElementById('clock')
 
-setInterval(function(){
+setInterval(function () {
     let date = new Date();
     clock.innerHTML = date.toLocaleTimeString();
 }, 1000);
