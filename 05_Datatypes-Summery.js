2006630c65771c2585c7143f23fda10ace17e0b9
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-08 16:08:55 +0530

    Update 05_Datatypes-Summery.js

--- 05_Datatypes-Summery.js
+++ 05_Datatypes-Summery.js
@@ -39,12 +39,12 @@ const anotherId = Symbol('123')
 
 const heros = ["shaktiman", "naagraj", "doga"]; // array
 
-let myObj = {    
+let myObj = {
     name: "Rajan",
     age: 24
 }  // Objects
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello world");
 }  // Functions
 
@@ -68,47 +68,47 @@ const myFunction = function(){
 let myYoutubename = "rajanSrivastava8899"
 
 let anothername = myYoutubename  // in this we are using copy of myYoutubename
-anothername="Chaiaurcode"
+anothername = "Chaiaurcode"
 
 console.log(myYoutubename);
 console.log(anothername);
 
 
 let userOne = {
-    email: "user@google.com",    
+    email: "user@google.com",
     upi: "user@ybl"
 }
 
 let userTwo = userOne  // in this we are using the reference value of userOne 
 
 userTwo.upi = "rajan@google.com", // userTwo.email (.email) is used for accessing the email value
- 
 
-console.log(userOne.email);
+
+    console.log(userOne.email);
 console.log(userTwo.email);
 
 
 
 
 let userOnes = {
-    email: "user@google.com",    
+    email: "user@google.com",
     upi: "user@ybl"
 }
 
 let userTwo = userOnes  // in this we are using the reference value of userOne 
 
 userTwo.upi = "Paytm", // userTwo.email (.email) is used for accessing the email value
- 
 
-console.log(userOnes.email);
+
+    console.log(userOnes.email);
 console.log(userTwo.upi);
 
 
 
 
 let userId = {
-    userName : "rajansri8899",
-    userPw : "lalaji123"
+    userName: "rajansri8899",
+    userPw: "lalaji123"
 }
 
 
