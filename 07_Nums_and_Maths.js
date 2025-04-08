4fb5b77e3d903c643dfd7b0fa68af9a486d4fc6f
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-08 16:11:53 +0530

    Update 07_Nums_and_Maths.js

--- 07_Nums_and_Maths.js
+++ 07_Nums_and_Maths.js
@@ -32,8 +32,8 @@ console.log(Math.round(4.6));
 console.log(Math.ceil(4.2));  // ceil is used for bada no (big no) so 5 will be ans
 console.log(Math.floor(4.9)); // floor is used for small no so 4 will be ans
 
-console.log(Math.min(1,2,3,4,5,6,88, 0, 20, 10));  // used to find smallest value
-console.log(Math.max(100, 99, 88, 10, 2, 1,79, 95));   // used to find largest value
+console.log(Math.min(1, 2, 3, 4, 5, 6, 88, 0, 20, 10));  // used to find smallest value
+console.log(Math.max(100, 99, 88, 10, 2, 1, 79, 95));   // used to find largest value
 
 
 console.log(Math.random());
@@ -42,9 +42,9 @@ console.log(Math.random() * 10 + 1);
 
 
 
-const min =110
-const max=150
+const min = 110
+const max = 150
 
-console.log(Math.floor(Math.random()* (max-min + 1))+min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
 //0 < x<1 => 0 < 41x < 41 => 11x = 40.23456890 => 41x+110<151
