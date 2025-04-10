3b2e7b5715ca178043c6dbaad1a5df60a4fc1dfe
Rajan Srivastava <74043759+rajanshrivastava88@users.noreply.github.com>
2025-04-10 19:32:35 +0530

    Update 30.2_Fetch_in_Javascript.js

--- 30.2_Fetch_in_Javascript.js
+++ 30.2_Fetch_in_Javascript.js
@@ -24,9 +24,10 @@ returning a promise that is fulfilled once the response is available.
 
 The promise resolves to the Response object representing the response to your request.
 
-A fetch() promise only rejects when the request fails, for example, because of a badly-formed request URL 
-or a network error. A fetch() promise does not reject if the server responds with HTTP status codes that 
-indicate errors (404, 504, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+A fetch() promise only rejects when the request fails, for example, because of a badly-formed 
+request URL or a network error. A fetch() promise does not reject if the server responds with 
+HTTP status codes that indicate errors (404, 504, etc.). Instead, a then() handler must check 
+the Response.ok and/or Response.status properties.
 
 
 */
